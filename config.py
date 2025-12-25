290e794741e575d974cd05dbfebf3f2edcec6809
Mohanish <127211714+TEIMohanish@users.noreply.github.com>
2025-12-25 09:42:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 class Config(object):
     API_ID = int(environ.get("API_ID", 21541476))
     API_HASH = environ.get("API_HASH", "a32789925b6d6d4f9c2661a2d98b78af")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "5550245504:AAGLNB1XkFfm0dkA54sULMv3Onrbc15VkjE")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "8393983881:AAH8XNEpz8PAD2LOoQDhHq5g3ZWUX5J78qw")
     #The bot will work only with following users
     AUTH_USERS = list(int(x) for x in environ.get("AUTH_USERS", "2023056811").split(" "))
     #Owner ID is the user id of your telegram account
